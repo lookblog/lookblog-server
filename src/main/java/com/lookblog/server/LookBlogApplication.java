a75d7f3a5585dab39aff4763225e60685d650bc6
Air <398225229@qq.com>
2019-10-02 10:56:53 +0800

    初始化提交

---
+++ src/main/java/com/lookblog/server/LookBlogApplication.java
@@ -0,0 +1,13 @@
+package com.lookblog.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookBlogApplication.class, args);
+    }
+
+}
